ac7198cd1132dd4d73da6c4ca06e5573e3b7ac0c
Else Meijerink <p.e.meijerink@gmail.com>
2021-11-25 17:35:37 +0000

    Added multipage navigation

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,17 @@
 // domain.com/news
 
+import { Fragment } from "react";
+
 function NewsPage() {
-    return <h1>News</h1>
+    return (
+        <Fragment>
+            <h1>News Page</h1>
+            <ul>
+                <li><a href="/news/article1"> Article 1 title</a></li>
+                <li><a href="/news/article2"> Article 2 title</a></li>
+            </ul>
+        </Fragment>
+        ) 
 }
 
 export default NewsPage; 
